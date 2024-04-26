5f818cadc776fd0304e950d0e98406f2c3568b10
{suharshit} <{suharshit123@gmail.com}>
2024-04-27 01:27:06 +0530

    1

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -25,7 +25,7 @@ const Projects = () => {
               <CardContent className="">
                 <div>
                   <a href="https://github.com/Suharshit/Eco-Millets-Hub-typescript" target="_blank">
-                    <video src="/eco-millets-hub.mp4" className="rounded-lg" autoPlay muted loop></video>
+                    <video src="/assets/eco-millets-hub.mp4" className="rounded-lg" autoPlay muted loop></video>
                   </a>
                 </div>
                 <div className="mt-2 flex space-x-3">
@@ -57,7 +57,7 @@ const Projects = () => {
               <CardContent className="">
                 <div>
                   <a href="https://github.com/Suharshit/Readaholics" target="_blank">
-                    <video src="/readaholic.mp4" className="rounded-lg" autoPlay muted loop></video>
+                    <video src="/assets/readaholic.mp4" className="rounded-lg" autoPlay muted loop></video>
                   </a>
                 </div>
                 <div className="mt-2 flex space-x-3">
@@ -85,7 +85,7 @@ const Projects = () => {
               <CardContent className="">
                 <div>
                   <a href="https://github.com/Suharshit/reyban-clone" target="_blank">
-                    <video src="/rayban.mp4" className="rounded-lg" autoPlay muted loop></video>
+                    <video src="/assets/rayban.mp4" className="rounded-lg" autoPlay muted loop></video>
                   </a>
                 </div>
                 <div className="mt-2 flex space-x-3">
